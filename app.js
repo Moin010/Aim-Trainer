aee943b2e79e5eb8f02db92235519e9da95bd50d
Moin Alam <moinalam010@gmail.com>
2024-03-08 21:58:18 +0600

    start button

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const target = document.querySelector("button.jumper")
 const hitCountr = document.querySelector(".h-score")
 const missCountr = document.querySelector(".m-score")
 const accuracySection = document.querySelector(".accuracy")
+// const startButton = document.querySelector(".startGameNow")
 
 
 
@@ -12,19 +13,24 @@ let hit=0;
 let miss=0;
 let gameStarted = 0;
 let accuracy = 0;
+let time =1;
 
 
 
 window.addEventListener("click", e=>{
    
-    //    target mover
-    targetMover(e);
+    //    score Calculator
+    scoreCalculator(e);
 
     // Score update
     scoreUpdater();
     
     // accuracy update
     accuracyCounter();
+
+    if(gameStarted==0){
+        moveTargetRandomly();
+    }
 
 })
 
@@ -42,12 +48,10 @@ scoreUpdater = () =>{
     hitCountr.innerHTML = `<i class="fa-solid fa-person-military-rifle"></i>HIT : ${hit} `
     missCountr.innerHTML = `<i class="fa-solid fa-person-military-rifle"></i>MISS : ${miss} `
 }
-//    target mover
-targetMover = (e) =>{
+//    score Calculator
+scoreCalculator = (e) =>{
     
     if(e.target.classList.contains("jumper")){
-        target.style.top= `${Math.round(Math.random() * 60+30)}%`;
-        target.style.left= `${Math.round(Math.random() * 80+10)}%`;
         hit++;
     }else{
         miss++;
@@ -56,12 +60,35 @@ targetMover = (e) =>{
 
 
 // timer
-startCounter = setInterval(()=>{
+// startCounter = setInterval(()=>{
+
+//     if(gameStarted){
+        
+//     }
 
 
 
+// }, 1000)
 
 
-}, 1000)
+moveTargetRandomly =()=>{  
+
+    let stopmoving = setInterval(()=>{
+        
+        gameStarted = 1;
+        target.style.top= `${Math.round(Math.random() * 60+30)}%`;
+        target.style.left= `${Math.round(Math.random() * 80+10)}%`;
+        console.log("moving")
+        
+        if(time==10){
+            clearInterval(stopmoving);
+            console.log("moving stoped")
+            gameStarted = 0;
+            console.log("g")
+        }
+
+        time++;
+    }, 1000)
+}
 
 
